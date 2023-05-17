d3cf43e4f495de62190d1a777d5e9d5ebc85f248
mik3se <mik3se@gmail.com>
2023-05-16 19:36:39 -0700

    FINAL COMMIT
    This time I really think- never mind, hope it is. Fixed a few typos and image desciptions.

--- include/aboutgroup.php
+++ include/aboutgroup.php
@@ -1,12 +1,12 @@
 <h2>Michael Reyes, Founder and Webmaster</h2>
-<img src="/images/APC_0030.JPG" width="640" alt="Michael Reyes"><br \>
+<img src="/images/APC_0030.JPG" width="640" alt="Image of Michael Reyes"><br \>
 <p>I'm the one who coded the HTML and CSS for the website, along with set up the mail server, shot the commercial, and did most of the management. I transferred ownership to William after I found out how great of a manager he is.</p>
 <h2>William Heng, CEO</h2>
-<img src="/images/APC_0043.JPG" width="640" alt="Michael Reyes"><br \>
-<p>I manage evevrything that happens in this company, from updating our website to shooting commercials.</p>
+<img src="/images/AAPC_0043.jpg" width="640" alt="Image of William Heng"><br \>
+<p>I manage everything that happens in this company, from updating our website to shooting commercials.</p>
 <h2>Aaron Meyer, Market Researcher</h2>
-<img src="/images/APC_0047.JPG" width="640" alt="Michael Reyes"><br \>
+<img src="/images/APC_0047.JPG" width="640" alt="Image of Aaron Meyer"><br \>
 <p>I listen to music and watch commercials to find out exactly what consumers would want out of our products. I also make sure we are able to beat competition in at least one aspect.</p>
 <h2>Oak Khant, Graphic Designer</h2>
-<img src="/images/APC_0021.JPG" width="640" alt="Michael Reyes"><br \>
+<img src="/images/APC_0021.JPG" width="640" alt="Image of Aaron Meyer"><br \>
 <p>I make sure everything looks good, from our logo to our fliers to our products.</p>
