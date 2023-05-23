d14ef7c688486fc850b2da948aebcd0b1fb58c7c
mik3se <mik3se@gmail.com>
2023-05-22 18:25:28 -0700

    impressive
    hmmph

--- include/links.php
+++ include/links.php
@@ -1,3 +1,4 @@
-<link rel="shortcut icon" type="/image/png" href="/images/logo.png"/>
-<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-<link href="/styles/style.css" rel="stylesheet" type="text/css">
+<meta charset="utf-8" />
+<link rel="shortcut icon" type="/image/png" href="/images/logo.png" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+<link href="/styles/style.css" rel="stylesheet" type="text/css" />
